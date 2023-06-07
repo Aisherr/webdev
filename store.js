6f8370bca07de02c40c68542a85ef5e81dd7a9cc
Aisherr <aishermohamed123@gmail.com>
2023-06-07 11:57:33 +0300

    This is a web-development project

---
+++ store.js
@@ -0,0 +1,109 @@
+document.getElementById('one').style.color="blue"
+if( document.readyState== 'loading'){
+     document.addEventListener('DOMContentLoaded', ready)
+    }else{
+        ready()
+    }
+function ready(){
+    var removeCartItemButtons= document.getElementsByClassName('btn-danger')
+    for(var i=0; i<removeCartItemButtons.length;i++){
+        var button= removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityinputs=document.getElementsByClassName('cart-quantity-input')
+    for(var i=0; i<quantityinputs.length;i++){
+        input.addEventListener('change', quantitychanged)
+    }
+    
+    var addToCartButtons= document.getElementsByClassName('shop-item-button')
+    for(var i=0;i<addToCartButtons.length;i++){
+        var button= addToCartButtons[i]
+        button.addEventListener('click',addToCartClicked)
+    }
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click',purchaseclicked)
+}
+
+function purchaseclicked(){
+    alert('Thank You for your Purchase')
+    var CartItems= document.getElementsByClassName('cart-items')[0]
+    while(CartItems.hasChildNodes()){
+        CartItems.removeChild(CartItems.firstChild)
+    }
+    UpdateCartTotal()
+}
+
+function removeCartItem(event){
+    var ButtonClicked =event.target
+    ButtonClicked.parentElement.parentElement.remove()
+    UpdateCartTotal()
+}
+function quantitychanged(event){
+    var input =event.target
+    if(isNaN(input.value) || input.value <=0){
+        input.value=1
+    }
+    UpdateCartTotal()
+}
+
+function addToCartClicked(event){
+    var button = event.target
+    var shopItem= button.parentElement.parentElement
+    var title=shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price=shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc=shopItem.getElementsByClassName('shop-item-image')[0].src
+    addItemToCart(title,price,imageSrc)
+
+    UpdateCartTotal()
+}
+
+function addItemToCart(title,price,imageSrc){
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems= document.getElementsByClassName('cart-items')[0]
+    var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+    for( var i=0; i<cartItemNames.length;i++){
+        if( cartItemNames[i].innerText==title){
+            alert('This item is already added to cart')
+            return
+        }
+    }
+
+    var cartRowContents=`
+       <div class="cart-item cart-column">
+           <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+           <span class="cart-item-title">${title}</span>
+       </div>
+       <span class="cart-price cart-column">${price}</span>
+       <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+       </div>`
+
+       cartRow.innerHTML =cartRowContents
+       cartItems.append(cartRow)
+       cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click',removeCartItem)
+
+       cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantitychanged)
+
+}
+function UpdateCartTotal(){
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows =cartItemContainer.getElementsByClassName('cart-row')
+    var total=0
+      for( var i=0; i< cartRows.length;i++){
+        var cartRow= cartRows[i]
+        var priceElement= cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement =cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('$' ,''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    
+      }
+total =Math.round(total *100)/100
+document.getElementsByClassName('cart-total-price')[0].innerText ='$'+ total
+
+}
+
+
+
